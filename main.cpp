c8c633f83189999b7caaeb46c3fe667147563a84
powerangelXD <692732163@qq.com>
2025-12-09 20:25:00 +0800

    refactor: Update example

--- main.cpp
+++ main.cpp
@@ -8,13 +8,12 @@
 #include "Compiler/Frontend/parser_base.hpp"
 #include "Compiler/Frontend/parser.hpp"
 
-// 示例程序源代码
 const std::string SOURCE_CODE = R"(
-WWWWi[1*3] - 9 != 99 && !MYID
+let VAR = "hello";
 )";
 
 int main() {
-    std::cout << "--- 源代码 ---\n" << SOURCE_CODE << "\n";
+    std::cout << "--- Source ---\n" << SOURCE_CODE << "\n";
     
     try {
         sakoraE::Lexer lexer(SOURCE_CODE);
@@ -24,7 +23,7 @@ int main() {
             std::cout << t.toString() << std::endl;
         }
 
-        auto result = sakoraE::WholeExprParser::parse(r.begin(), r.end());
+        auto result = sakoraE::StatementParser::parse(r.begin(), r.end());
 
         if (result.status == sakoraE::ParseStatus::FAILED)
             sutils::reportError(sakoraE::OccurredTerm::PARSER, "Parsering Failed...", {result.end->info.line, result.end->info.column, "system parser error"});
