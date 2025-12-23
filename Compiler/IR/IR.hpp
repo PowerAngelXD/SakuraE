aba35ba3870f4c5d8720c49a9b158bf393170151
powerangelXD <692732163@qq.com>
2025-12-23 13:22:54 +0800

    feat

---
+++ Compiler/IR/IR.hpp
@@ -0,0 +1,13 @@
+#ifndef SAKORA_IR_HPP
+#define SAKORA_IR_HPP
+
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/Module.h>
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/Verifier.h>
+
+namespace sakIR {
+    
+}
+
+#endif // !SAKORA_IR_HPP
