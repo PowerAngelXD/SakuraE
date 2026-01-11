d761f2d970401a3b3ad723063614ea1248d20bc5
powerangelXD <692732163@qq.com>
2026-01-11 22:55:52 +0800

    feat: Basically finish some generator

--- Compiler/CodeGen/generator.cpp
+++ Compiler/CodeGen/generator.cpp
@@ -36,14 +36,42 @@ namespace sakoraE::CodeGen {
     }
 
     llvm::Value* CodeGenerator::genLiteralNode(NodePtr node) {
-        return nullptr;
+        if (!node->isLeaf()) 
+            throw SakoraError(OccurredTerm::IR_GENERATING, 
+                            "This Node is not a leaf-node",
+                            node->getToken().info);
+        
+        auto token = node->getToken();
+        switch (token.type) {
+            case TokenType::INT_N:
+                return llvm::ConstantInt::get(llvm::Type::getInt32Ty(*context), atoi(token.content.c_str()));
+            case TokenType::FLOAT_N:
+                return llvm::ConstantFP::get(llvm::Type::getFloatTy(*context), atof(token.content.c_str()));
+            case TokenType::BOOL_CONST:
+                return llvm::ConstantInt::get(llvm::Type::getInt1Ty(*context), (token.content == "true"?true:false));
+            case TokenType::STRING:
+                return llvm::ConstantDataArray::getString(*context, token.content.c_str());
+            case TokenType::CHAR:
+                return llvm::ConstantInt::get(llvm::Type::getInt8Ty(*context), token.content.at(0));
+            default:
+                throw SakoraError(OccurredTerm::IR_GENERATING, 
+                            "Unknown literal",
+                            node->getToken().info);
+        }
     }
 
     llvm::Value* CodeGenerator::genIndexOpNode(NodePtr node) {
-        return nullptr;
+        return genAddExprNode((*node)[ASTTag::HeadExpr]);
     }
 
     llvm::Value* CodeGenerator::genCallingOpNode(NodePtr node) {
+        if (!node->hasNode(ASTTag::Exprs)) return nullptr;
+
+        std::vector<llvm::Value*> args;
+        for (auto expr: node->getChildren()) {
+            args.push_back(genWholeExprNode(expr));
+        }
+
         return nullptr;
     }
 
