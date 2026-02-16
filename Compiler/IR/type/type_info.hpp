828bcd5d7f565aef79b981e79f41f71bc3f30a63
fzsgball <fzsgball@qq.com>
2026-02-16 18:47:51 +0800

    refactor: TypeInfo structure

--- Compiler/IR/type/type_info.hpp
+++ Compiler/IR/type/type_info.hpp
@@ -2,7 +2,10 @@
 #define SAKURAE_TYPE_INFO_HPP
 
 #include <map>
+#include <stdexcept>
+#include <variant>
 
+#include "Compiler/Utils/Logger.hpp"
 #include "type.hpp"
 #include "Compiler/Error/error.hpp"
 
@@ -21,7 +24,9 @@ namespace sakuraE::IR {
         Null,
         Custom,
         // Structure
-        Array
+        Array,
+        Pointer,
+        Ref
     };
 
     inline IRType* tid2IRType(TypeID tid) {
@@ -52,50 +57,78 @@ namespace sakuraE::IR {
         }
     }
 
+    class TypeInfo;
+
+    class ArrayTypeInfo {
+        std::vector<TypeInfo*> elementTypes;
+    public:
+        ArrayTypeInfo(std::vector<TypeInfo*> elements): elementTypes(elements) {}
+        
+        std::size_t length() { return elementTypes.size(); }
+        TypeInfo* getElementTy() { return elementTypes[0]; }
+    };
+
+    class PointerTypeInfo {
+        TypeInfo* elementType;
+    public:
+        PointerTypeInfo(TypeInfo* element): elementType(element) {}
+
+        TypeInfo* getElementTy() { return elementType; }
+    };
+
+    class RefTypeInfo {
+        TypeInfo* elementType;
+    public:
+        RefTypeInfo(TypeInfo* element): elementType(element) {}
+
+        TypeInfo* getElementTy() { return elementType; }
+    };
+
     class TypeInfo {
         TypeID typeID;
         
-        bool isArrayType = false;
-        std::vector<TypeInfo*> elementTypes;
+        std::variant<
+            std::monostate,
+            ArrayTypeInfo,
+            PointerTypeInfo,
+            RefTypeInfo
+        > complexTypeInfo;
     public:
         TypeInfo(TypeID tid): typeID(tid) {}
 
         TypeInfo(std::vector<TypeInfo*> tids): 
-            typeID(Array), isArrayType(true), elementTypes(tids) {}
+            typeID(Array), complexTypeInfo(ArrayTypeInfo(tids)) {}
+        
+        TypeInfo(TypeID id, TypeInfo* elemntTid): 
+            typeID(id), complexTypeInfo([&]() -> std::variant<std::monostate, ArrayTypeInfo, PointerTypeInfo, RefTypeInfo> {
+                switch (id) {
+                    case Pointer: return PointerTypeInfo(elemntTid);
+                    case Ref: return RefTypeInfo(elemntTid);
+                    default: throw std::runtime_error("Cannot use other typeid to create single element typeinfo");
+                }
+            }()) {}
         
         ~TypeInfo()=default;
 
-        bool isArray() {
-            return isArrayType;
-        }
-
-        const TypeID& getTypeID() {
-            return typeID;
-        }
-
-        const std::vector<TypeInfo*>& getTypeArray() {
-            if (isArray())
-                return elementTypes;
-            else
-                throw SakuraError(OccurredTerm::IR_GENERATING,
-                                "Expected to get a non-array-type TypeInfo as a array-type",
-                                {0, 0, "InsideProblem"});
-        }
+        bool isArray() { return typeID == Array; }
+        bool isPointer() { return typeID == Pointer; }
+        bool isRef() { return typeID == Ref; }
+        const TypeID& getTypeID() { return typeID; }
 
         IRType* toIRType() {
-            if (isArrayType) {
-                IRType* elemType = nullptr;
-
-                if (elementTypes[0]->isArray()) {
-                    elemType = elementTypes[0]->toIRType();
-                } else {
-                    elemType = tid2IRType(elementTypes[0]->typeID);
-                }
-        
-                return IRType::getArrayTy(elemType, elementTypes.size());
+            if (isArray()) {
+                auto arrTy = std::get<ArrayTypeInfo>(complexTypeInfo);
+                return IRType::getArrayTy(arrTy.getElementTy()->toIRType(), arrTy.length());
             }
-
-            return tid2IRType(typeID);
+            else if (isPointer()) {
+                auto ptrTy = std::get<PointerTypeInfo>(complexTypeInfo);
+                return IRType::getPointerTo(ptrTy.getElementTy()->toIRType());
+            }
+            else if (isRef()) {
+                auto refTy = std::get<RefTypeInfo>(complexTypeInfo);
+                return IRType::getPointerTo(refTy.getElementTy()->toIRType());
+            }
+            else return tid2IRType(typeID);
         }
 
         static TypeInfo* makeTypeID(TypeID typeID);
