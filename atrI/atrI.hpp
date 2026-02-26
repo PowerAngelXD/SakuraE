7f2e2059c79dd5960a016434e08c5ae98c24d42e
fzsgball <fzsgball@qq.com>
2026-02-26 11:15:18 +0800

    test

--- atrI/atrI.hpp
+++ atrI/atrI.hpp
@@ -43,7 +43,7 @@ namespace atri {
                     parseCommand(list[0], args);
 #else
                     std::cout << "RUNNING DEBUG MODE, TEST PROGRAM: " << std::endl;
-                    parseCommand("run", {"test3.sak", "-sakir", "-llvmir"});
+                    parseCommand("run", {"test3.sak", "-sakir", "-llvmir", "-rawllvm"});
                     parseCommand("exit", {});
 #endif
                 }
