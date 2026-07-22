6f94557681901690cf55302310a39da70e06b4b8
powerangelXD <692732163@qq.com>
2026-07-22 10:55:34 +0800

    feat(zed): improve SakuraE syntax highlighting

--- sakurae-zed/languages/sakurae/highlights.scm
+++ sakurae-zed/languages/sakurae/highlights.scm
@@ -1,9 +1,13 @@
 [
-  "if" "else" "for" "while"
-  "return" "break" "continue"
-  "let" "func" "range" "repeat" "match" "default" "ref"
-  "i32" "i64" "ui32" "ui64" "f32" "f64" "bool" "char" "string" "void"
+  "if" "else" "while" "for" "func"
+  "return" "let" "const" "range"
+  "break" "continue" "match" "repeat"
+  "struct" "impl" "is" "typeof" "ref" "default" "sizeof"
 ] @keyword
+
+[
+  "i32" "i64" "ui32" "ui64" "f32" "f64" "bool" "char" "string" "void"
+] @type
 
 (number) @constant.numeric
 (string) @string
@@ -11,6 +15,10 @@
 (boolean) @constant.builtin.boolean
 
 (identifier) @variable
+
+((identifier) @keyword
+  (#match? @keyword "^(const|struct|impl|is|typeof|sizeof)$"))
+
 (func_define_stmt "func" @keyword)
 (func_define_stmt (identifier) @function)
 (atom_identifier_expr (identifier) @function.call (calling_op))
